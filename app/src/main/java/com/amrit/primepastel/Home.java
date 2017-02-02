cf3f7abcbffe07879a679ea417f028051d69dbab
Amrit Ghimire <iamritghimire@gmail.com>
2017-02-02 22:49:10 +0545

    Start page

---
+++ app/src/main/java/com/amrit/primepastel/Home.java
@@ -0,0 +1,123 @@
+package com.amrit.primepastel;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Home.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Home#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Home extends Fragment {
+
+    int currentPage;
+    int[] lists=new int[]{R.drawable.images, R.drawable.cakeone, R.drawable.caketwo, R.drawable.cakethree, R.drawable.cakefive};
+
+    private OnFragmentInteractionListener mListener;
+
+    public Home() {
+
+        // Required empty public constructor
+    }
+
+
+    public static Home newInstance() {
+        Home fragment = new Home();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Activity context = getActivity();
+        View view=inflater.inflate(R.layout.fragment_home,container,false);
+        final ViewPager viewPager=((ViewPager)view.findViewById(R.id.homepage_slider));
+        AndroidImageAdapter androidImageAdapter = new AndroidImageAdapter(context,lists);
+        viewPager.setAdapter(androidImageAdapter);
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabDots);
+        tabLayout.setupWithViewPager(viewPager, true);
+
+        final Handler handler = new Handler();
+
+        final Runnable update = new Runnable() {
+            public void run() {
+                if (currentPage == lists.length - 1) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+            }
+        };
+
+
+        new Timer().schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                handler.post(update);
+            }
+        }, 500, 3000);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
